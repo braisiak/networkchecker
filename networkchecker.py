13b4b1c003611c7f1f9a7b420aeb641f6fe8e76f
braisiak <smvsone@gmail.com>
2021-11-19 15:52:56 +0100

    Created repository

---
+++ networkchecker.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+import socket
+from requests import get
+import subprocess
+
+app = Flask(__name__)
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+public_ip = get('https://api.ipify.org').text
+
+@app.route('/')
+def index():
+    return render_template('index.html', variable = local_ip, variable2 = public_ip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ run.bat
@@ -0,0 +1,4 @@
+@ECHO off
+call .\env\Scripts\activate
+call python networkchecker.py
+
